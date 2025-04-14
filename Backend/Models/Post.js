135c57759a11a27497703b76f28433fb932cc461
ameni rahmouni <amanirahmouni2000@gmail.com>
2025-04-14 21:58:32 +0100

    qqle modif au niveau de modele

--- Backend/Models/Post.js
+++ Backend/Models/Post.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   idAuteur: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  //idPost: { type: mongoose.Schema.Types.ObjectId },
-  //idCommentaire: [{ type: mongoose.Schema.Types.ObjectId }],
+  idPost: { type: mongoose.Schema.Types.ObjectId },
+  idCommentaire: [{ type: mongoose.Schema.Types.ObjectId }],
   titre: String,
   contenu: String,
   date_creation:Date,
